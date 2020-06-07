6b0a2d86f106d634070a6513f8cfb5bac1d64c04
KIM MINSEONG <34212270+mincastle98@users.noreply.github.com>
2020-06-07 18:52:21 +0900

    Update truckOnBridge.cpp

--- Programmers/Level 2/truckOnBridge.cpp
+++ Programmers/Level 2/truckOnBridge.cpp
@@ -1,4 +1,4 @@
-//2020.06.07
+//2020.06.07 (수정)
 //프로그래머스 다리를 지나는 트럭 (큐)
 //By Mincastle
 
@@ -8,60 +8,48 @@
 
 using namespace std;
 
-queue<int> queuePlus1(queue<int> q) {
-    queue<int> temp;
-    int n = q.size();
-    for (int i = 0; i < n; i++) {
-        temp.push(q.front() + 1);
-        q.pop();
+class Truck {
+public:
+    int outTime;
+    int weight;
+
+    Truck(int outTime, int weight) {
+        this->outTime = outTime;
+        this->weight = weight;
     }
-    return temp;
-}
+};
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 1;
-    queue<int> onBridge;
+    int answer;
+    int time = 0;
+    queue<Truck> onBridge;
     int now_weight = 0;
-    int check;
-    queue<int> sameCnt;
     reverse(truck_weights.begin(), truck_weights.end());
 
     while (!truck_weights.empty() || !onBridge.empty()) {
+        time++;
+        if (!onBridge.empty() && onBridge.front().outTime == time) {
+            now_weight -= onBridge.front().weight;
+            onBridge.pop();
+        }
+
         if (!truck_weights.empty()) {
-            int wait_truck = truck_weights.back();
-            if (now_weight + wait_truck <= weight) {
-                now_weight += wait_truck;
+            if (now_weight + truck_weights.back() <= weight) {
+                onBridge.push(Truck(time + bridge_length, truck_weights.back()));
+                now_weight += truck_weights.back();
                 truck_weights.pop_back();
-                if (wait_truck == truck_weights.back())
-                    sameCnt.push(1);
-                for (int i = 0; i < bridge_length - onBridge.size();) {
-                    onBridge.push(wait_truck);
-                }
             }
         }
-
-        check = onBridge.front();
-        onBridge.pop();
-
-        if (onBridge.front() != check) {
-            now_weight -= check;
-        }
-        if (!sameCnt.empty() && sameCnt.front() == bridge_length) {
-            now_weight -= check;
-            sameCnt.pop();
-        }
-
-        sameCnt = queuePlus1(sameCnt);
-        answer++;
     }
 
+    answer = time;
     return answer;
 }
 
 int main(void) {
-    vector<int> truck_weight = {1, 2, 3, 4, 4, 3, 1};
+    vector<int> truck_weight = {7, 4, 5, 6};
 
-    solution(8, 18, truck_weight);
+    solution(2, 10, truck_weight);
 
     return 0;
 }
