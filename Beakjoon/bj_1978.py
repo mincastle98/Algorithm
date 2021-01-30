be35cbc1c098d2a0ae7275f859532eed635a96f5
mincastle98 <alstjd98522@naver.com>
2021-01-30 15:30:29 +0900

    21/01/03 solved 1978(findPrimeNumber) with Python3

---
+++ Beakjoon/bj_1978.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+
+def solution(num):
+    answer = 0
+    for i in num:
+        flag = 1
+
+        if i == 1:
+            continue
+        for j in range(2, int(sqrt(i)) + 1):
+            if i % j == 0:
+                flag = 0
+                break
+
+        if flag:
+            answer += 1
+    print(answer)
+
+
+n = int(input())
+inputs = list(map(int, input().split(" ")))
+inputs = inputs[:n]
+solution(inputs)
