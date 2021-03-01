3daa36b6cd41b46e7e4507be467eb7d1bb4beb0c
mincastle98 <alstjd98522@naver.com>
2021-03-01 17:09:10 +0900

    21/03/03 solved 124world(124 나라의 숫자) with Python3

---
+++ Programmers/Practice/Level 2/124world.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = ''
+
+    while n:
+        if n % 3 == 0:
+            answer = '4' + answer
+            n -= 1
+        elif n % 3 == 1:
+            answer = '1' + answer
+        elif n % 3 == 2:
+            answer = '2' + answer
+        n //= 3
+
+    return answer
