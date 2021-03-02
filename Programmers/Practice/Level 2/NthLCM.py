8540125b24a84e789a9ab22e5044474eaf405416
mincastle98 <alstjd98522@naver.com>
2021-03-02 18:07:49 +0900

    21/03/02 solved NthLCM(N개의 최소공배수) with Python3

---
+++ Programmers/Practice/Level 2/NthLCM.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    answer = 1
+
+    arr.sort()
+    for a in arr:
+        answer *= a
+        for j in range(len(arr)):
+            if arr[j] % a == 0:
+                arr[j] //= a
+
+    return answer
+
+
+print(solution([2, 6, 8, 14]))
