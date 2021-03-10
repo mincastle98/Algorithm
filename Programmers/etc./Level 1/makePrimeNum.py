ef90f0d5eba7778c50b5b16c84f90f9075d50b90
mincastle98 <alstjd98522@naver.com>
2021-03-10 23:58:28 +0900

    21/03/10 solved makePrimeNum(소수 만들기) with Python3

---
+++ Programmers/etc./Level 1/makePrimeNum.py
@@ -0,0 +1,22 @@
+# 소수 만들기
+from itertools import combinations
+import math
+
+
+def solution(nums):
+    answer = -1
+    combList = list(combinations(nums, 3))
+    for c in combList:
+        tmp = sum(c)
+        flag = True
+        for i in range(2, math.ceil(math.sqrt(tmp)) + 1):
+            if not tmp % i:
+                flag = False
+                break
+        if flag:
+            answer += 1
+
+    return answer + 1
+
+
+print(solution([1, 2, 7, 6, 4]))
