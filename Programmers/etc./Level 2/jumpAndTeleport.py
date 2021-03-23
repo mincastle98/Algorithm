fcc4857679e5edaade4a5fb8e61c35c0892e9b93
mincastle98 <alstjd98522@naver.com>
2021-03-23 23:11:19 +0900

    21/03/23 solved jumpAndTeleport(점프와 순간 이동) with Python3

--- Programmers/etc./Level 2/jumpAndTeleport.py
+++ Programmers/etc./Level 2/jumpAndTeleport.py
@@ -3,12 +3,10 @@ def solution(n):
     answer = 0
 
     while n > 0:
-        if n % 2 == 0:
-            n //= 2
-        else:
+        if n % 2 != 0:
             n -= 1
             answer += 1
-            n // 2
+        n //= 2
 
     return answer
 
