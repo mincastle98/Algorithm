ae200d2779785673f5af99f7ba1cf04cd8761aed
mincastle98 <alstjd98522@naver.com>
2022-02-05 14:33:23 +0900

    22/02/05 solved BOJ_11279(최대 힙, S3)

---
+++ Beakjoon/DataStructure/BOJ_11279_최대 힙.py
@@ -0,0 +1,19 @@
+# 11279 - 최대 힙(S3)
+import sys
+import heapq
+
+N = int(input())
+
+ops = []
+for _ in range(N):
+    ops.append(int(sys.stdin.readline()))
+
+max_heap = []
+for op in ops:
+    if op:
+        heapq.heappush(max_heap, -op)
+    else:
+        try:
+            print(-heapq.heappop(max_heap))
+        except:
+            print(0)
