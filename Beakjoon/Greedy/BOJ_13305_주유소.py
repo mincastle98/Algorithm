e5a8477c35699a274d5545acee3c16748c9f8e1c
mincastle98 <alstjd98522@naver.com>
2022-03-08 17:42:01 +0900

    22/03/08 solved BOJ_13305(주유소, S4)

--- Beakjoon/Greedy/BOJ_13305_주유소.py
+++ Beakjoon/Greedy/BOJ_13305_주유소.py
@@ -2,5 +2,16 @@
 import sys
 
 N = int(sys.stdin.readline())
-dist = list(map(int, sys.stdin.readline().split()))
+dists = list(map(int, sys.stdin.readline().split()))
 stations = list(map(int, sys.stdin.readline().split()))
+
+i = 0
+price = 0
+while i < len(stations)-1:
+    j = 1
+    while i+j < len(stations) and stations[i] < stations[i+j]:
+        j += 1
+    price += stations[i] * sum(dists[i:i+j])
+    i = i+j
+
+print(price)
