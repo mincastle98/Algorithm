314d5472ef336db10f6a0596315d6503916323d9
mincastle98 <alstjd98522@naver.com>
2022-05-01 23:40:59 +0900

    22/05/01 solving BOJ_11728(배열 합치기, S5)

---
+++ Beakjoon/Two Pointer/BOJ_11728_배열 합치기.py
@@ -0,0 +1,10 @@
+# 11728 - 배열 합치기(S5)
+N, M = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+new_list = []
+
+idx_a, idx_b = 0, 0
+while idx_a < N and idx_b < M:
+    if
