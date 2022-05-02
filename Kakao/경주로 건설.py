760f2c858e68640eef4c7b9d7a28c43b3f1ae364
mincastle98 <alstjd98522@naver.com>
2022-05-02 20:53:35 +0900

    22/05/02 solving 경주로 건설(Level 3)

---
+++ Kakao/경주로 건설.py
@@ -0,0 +1,37 @@
+# 경주로 건설(Level 3)
+def check_yx(yx, size):
+    return 0 <= yx[0] < size and 0 <= yx[1] < size
+
+
+def solution(board):
+    answer = 0
+
+    board_size = len(board)
+    dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+    stack = [{"yx": [0, 0], "cost": 0, "visited": [item[:] for item in board], "dir": -1}]
+    stack[0]["visited"][0][0] = 1
+    while stack:
+        now = stack.pop()
+        if now["yx"] == [board_size - 1, board_size - 1]:
+            answer = (min(answer, now["cost"]) if answer != 0 else now["cost"])
+        else:
+            for i, d in enumerate(dirs):
+                next = dict()
+                next["yx"] = [now["yx"][0] + d[0], now["yx"][1] + d[1]]
+                y, x = next["yx"]
+                if check_yx(next["yx"], board_size) and now["visited"][y][x] == 0:
+                    if now["dir"] == -1 or now["dir"] == i:
+                        next["cost"] = now["cost"] + 100
+                    else:
+                        next["cost"] = now["cost"] + 600
+                    next["visited"] = [item[:] for item in now["visited"]]
+                    next["visited"][y][x] = 1
+                    next["dir"] = i
+
+                    stack.append(next)
+
+    return answer
+
+
+print(solution([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
