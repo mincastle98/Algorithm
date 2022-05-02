b8512831524d93e284f8d8f2aea2048f1bbbcd3e
mincastle98 <alstjd98522@naver.com>
2022-05-02 14:19:03 +0900

    22/05/02 solved BOJ_11728(배열 합치기, S5)

--- Beakjoon/Two Pointer/BOJ_11728_배열 합치기.py
+++ Beakjoon/Two Pointer/BOJ_11728_배열 합치기.py
@@ -1,10 +1,23 @@
 # 11728 - 배열 합치기(S5)
-N, M = int(input())
+N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 new_list = []
 
 idx_a, idx_b = 0, 0
-while idx_a < N and idx_b < M:
-    if
+while idx_a < N or idx_b < M:
+    if idx_a == N and idx_b < M:
+        new_list.append(B[idx_b])
+        idx_b += 1
+    elif idx_a < N and idx_b == M:
+        new_list.append(A[idx_a])
+        idx_a += 1
+    elif A[idx_a] <= B[idx_b]:
+        new_list.append(A[idx_a])
+        idx_a += 1
+    else:
+        new_list.append(B[idx_b])
+        idx_b += 1
+
+print(*new_list)
