73c18b1d87ae018d9d8281fc62ab8b8d1ae8faa1
mincastle98 <alstjd98522@naver.com>
2022-05-06 23:19:47 +0900

    22/05/06 solved BOJ_11053(가장 긴 증가하는 부분 수열, S2)

--- Beakjoon/Dynamic Programming/BOJ_11053_가장 긴 증가하는 부분 수열.py
+++ Beakjoon/Dynamic Programming/BOJ_11053_가장 긴 증가하는 부분 수열.py
@@ -2,13 +2,12 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-dp = [0] * N
-dp[-1] = 1
+dp = [1] * N
 
-for i in range(N-2, -1, -1):
-    if A[i] < A[i+1]:
-        dp[i] = dp[i+1]+1
-    else:
-        dp[i] = dp[i+1]
+for i in range(1, N):
+    for j in range(0, i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-print(dp[0])
+print(max(dp))
+print(dp)
